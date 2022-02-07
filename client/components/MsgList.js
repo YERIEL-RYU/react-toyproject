d0f39a2679ee9f0790ae551958612d8a5f5018f7
YERIEL-RYU <yeriel.ryu@gmail.com>
2022-02-07 14:03:30 +0900

    2강 server side rendering

--- client/components/MsgList.js
+++ client/components/MsgList.js
@@ -5,11 +5,11 @@ import useInfiniteScroll from "../hooks/useInfiniteScroll"
 import { useRouter } from "next/router"
 import fetcher from '..fetcher.js'
 
-const MsgList = () => {
+const MsgList = ({smsgs, users}) => {
   const {
     query: { userId = ''}
   } = useRouter()
-  const [msgs, setMsgs] = useState([])
+  const [msgs, setMsgs] = useState(smsgs)
   const [editingId, setEditingId] = useState(null)
   const [hasNext, setHasNext] = useState(true)
   const fetchMoreEl = useRef(null)
@@ -71,6 +71,7 @@ const MsgList = () => {
             onDelete={()=> onDelete(x.id)}
             isEditing={editingId === x.id}
             myId={userId}
+            user={users[x.userId]}
           />
         ))}
       </ul>
